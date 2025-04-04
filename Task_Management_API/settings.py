aef0427a7bad7e4ff2e0437757c1f45559640756
Bayisa Daba <bayisa615@gmail.com>
2025-04-04 16:44:43 +0300

    modified django_error and sqlite

--- Task_Management_API/settings.py
+++ Task_Management_API/settings.py
@@ -186,4 +186,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'bayisa615@gmail.com'
-EMAIL_HOST_PASSWORD = 'wxdkeeyqsgcfnxog' 
+EMAIL_HOST_PASSWORD = 'wykdcgxiyujiuyrc' 
